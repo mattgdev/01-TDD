7dac53c86d0b413692dfcefaf535da56b27967ec
Matthew Gordon <matthewgordon89@gmail.com>
2018-06-13 16:31:05 -0700

    Testing in progress

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const greet = require('./../src/lib/greet');
+const greet = require('../lib/greet');
 
 describe('testing #greet', () => {
   describe('testing #greet.hi', () => {
     test('greet.hi should return Hello Judy', () => {
-      expect(greet.hi('Judy')).toEqual('Hello Judy!');
+      expect(greet.hi('Judy')).toEqual('hello Judy');
     });
 
     // this is an error checking test
     test('should return -1 in case of no input', () => {
-      expect(greet.hi('')).toEqual(-1);
-      expect(greet.hi()).toEqual(-1);
+      expect(greet.hi('')).toBeNull();
+      expect(greet.hi()).toBeNull();
     });
   });
 })
