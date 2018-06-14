d10dc22e12f6eeecdb4b30278c9a3affb7f50302
Matthew Gordon <matthewgordon89@gmail.com>
2018-06-14 13:33:29 -0700

    Finished

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -1,17 +1,27 @@
 'use strict';
 
 const greet = require('../lib/greet');
+const math = require('../lib/arithmetic');
 
 describe('testing #greet', () => {
   describe('testing #greet.hi', () => {
     test('greet.hi should return Hello Judy', () => {
-      expect(greet.hi('Judy')).toEqual('hello Judy');
+      expect(greet.hi('world')).toEqual('hello world');
     });
 
-    // this is an error checking test
-    test('should return -1 in case of no input', () => {
+    test('should return null in case of no input', () => {
       expect(greet.hi('')).toBeNull();
-      expect(greet.hi()).toBeNull();
     });
   });
-})
+
+  describe('testing #greet.add', () => {
+    test('math.add should return the sum of x and y', () => {
+      expect(math.add(3, 5)).toEqual(8);
+    });
+
+    test('math.sub should return the sum of x and y', () => {
+      expect(math.sub(5, 5)).toEqual(0);
+    });
+  });
+});
+
